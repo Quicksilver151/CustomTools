cb3367ee2e51e4490328b7ae61817d5c4f6a222f
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2024-03-24 17:54:34 +0800

    polbarwal more argumetns ig

--- wal/polywal.sh
+++ wal/polywal.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-polybarwal $1 && neofetch && pywalfox update && set_discord_bg_link_wal && godot4_wal && okular_wal;
+polybarwal $* && neofetch && pywalfox update && set_discord_bg_link_wal && godot4_wal && okular_wal;
 cp -f ~/.cache/wal/zelkBD.theme.css ~/.config/BetterDiscord/themes/
 
 #set_firefox_tabliss_wal &&
