7a5e4080fcb976adbcd41bd45a6c2a153be66d4f
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2024-03-26 12:11:42 +0800

    recent to top rofi obsidian

--- useful extras/obsidian_rofi.py
+++ useful extras/obsidian_rofi.py
@@ -31,11 +31,19 @@ def get_vaults() -> list:
     obsidian_json_path = platformdirs.user_config_dir("obsidian") + "/obsidian.json"
     obsidian_json_file = open(obsidian_json_path,"r")
     json_data:dict = json.loads(obsidian_json_file.read())
-
+    
     vault_list: list[str] = []
+
     for vault_id in json_data["vaults"].keys():
+        is_open = json_data["vaults"][vault_id].__contains__("open")
+        
         vault_name: str = json_data["vaults"][vault_id]["path"].split("/")[-1]
-        vault_list.append(vault_name)
+        if is_open:
+            vault_list.reverse()
+            vault_list.append(vault_name)
+            vault_list.reverse()
+        else:
+            vault_list.append(vault_name)
 
 
     return vault_list
