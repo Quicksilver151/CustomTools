b761a061f4117b7d9b9a22652f24d8c1ee536e55
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2024-04-28 10:23:32 +0800

    validate quick access paths

--- wm management/quick-access.py
+++ wm management/quick-access.py
@@ -8,31 +8,37 @@ current_desktop = subprocess.getoutput("wmctrl -d | grep '*' | cut -d ' ' -f1")
 desktops_with_dolphin = subprocess.getoutput("wmctrl -l | grep Dolphin | awk '{print $2}' ORS=' '").split()
 
 
+
+def valid_dirs(dir: str) -> bool:
+    return dir.split('.').__len__() < 2
+
 def main():
     # start rofi instance:
     r = Rofi()
 
     # get directories
     raw_options: list[str] = os.listdir(os.path.expanduser("~/QuickAccess"))
-    options = []
+    filtered_options = list(filter(valid_dirs, raw_options))
     
-    for option in raw_options:
-        options.append(option.replace(" ","\ "))
+    options = []
+    for option in filtered_options:
+        options.append(option.replace(" ","\\ "))
 
     print(options)
     options_lower: list = []
 
     for i in range(len(options)):
-        options_lower.append(raw_options[i].lower())
+        options_lower.append(filtered_options[i].lower())
 
     # options = ['Downloads', 'Home', 'Videos', 'Saves', 'Documents', 'Anicli']
     index, key = r.select('', options_lower)
     print(index,key)
     # open folder
-    if key == -1:
+    if -1 in [index, key]:
         return
     
     os.system("nohup dolphin --new-window ~/QuickAccess/"+str(options[index])+" &")
+
 
 
 if __name__ == "__main__":
