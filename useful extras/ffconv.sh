6a3bc902f06dc506451cdd595c2a2f36644b8d11
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2026-03-13 14:03:35 +0500

    fixed weird ffconv bug

--- useful extras/ffconv.sh
+++ useful extras/ffconv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in *.$1;
-do
-  echo ffmpeg -i "$i" ${@:3} "$(basename $i .$1).$2";
-  ffmpeg -i "$i" ${@:3} "$(basename $i .$1).$2";
+for i in *."$1"; do
+  out="${i%.$1}.$2"
+  echo ffmpeg -i "$i" ${@:3} "$out"
+  ffmpeg -i "$i" ${@:3} "$out"
 done
 
