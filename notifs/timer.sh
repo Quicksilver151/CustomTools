744cc26dd1390bae6f28069e505d9010de171263
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2026-03-23 11:58:11 +0500

    updated termdown ver

--- notifs/timer.sh
+++ notifs/timer.sh
@@ -2,5 +2,5 @@
 time="$1"
 shift 1
 msg="$@"
-termdown "$time" -T "$msg" --time-format "%H:%M:%S" --no-figlet-y-offset 0; notify-all --urgency=critical -a "timer" "$msg"
+termdown "$time" -T "$msg" --time-format "%H:%M:%S"; notify-all --urgency=critical -a "timer" "$msg"
 
